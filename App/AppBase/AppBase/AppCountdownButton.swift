a83ab29ca13da2aec47fa2a39cd540bc40ad97c4
hxs <lychae@foxmail.com>
2020-04-13 15:46:13 +0800

    添加倒计时按钮

--- App/AppBase/AppBase/AppCountdownButton.swift
+++ App/AppBase/AppBase/AppCountdownButton.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-public class AppCountdownButton: UIButton {
+open class AppCountdownButton: UIButton {
 
     var timer: Timer?
-    public var countDownTimeInterval: Int = 60
-    public var finshTitle: String = "重新获取"
-    public var changeTitle: String = "s"
+    open var countDownTimeInterval: Int = 60
+    open var finshTitle: String = "重新获取"
+    open var changeTitle: String = "s"
     
     deinit {
-        print("\(classForCoder)" + " 释放")
+        print("\(classForCoder)" + " deinit")
     }
     
-    public override func removeFromSuperview() {
+    open override func removeFromSuperview() {
         stopCountdown()
         super.removeFromSuperview()
     }
@@ -27,7 +27,7 @@ public class AppCountdownButton: UIButton {
     // MARK: - Timer
     
     /// 倒计时
-    @objc private func countDown() {
+    @objc func countDown() {
         countDownTimeInterval -= 1
         if countDownTimeInterval == 0 {
             stopCountdown()
@@ -37,7 +37,7 @@ public class AppCountdownButton: UIButton {
     }
     
     /// 开始倒计时
-    public func startCountdown(second: Int = 60) {
+    open func startCountdown(second: Int = 60) {
         timer?.invalidate()
         countDownTimeInterval = second
         timer = Timer(timeInterval: 1.0, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
@@ -47,7 +47,7 @@ public class AppCountdownButton: UIButton {
     }
     
     /// 倒计时结束
-    public func stopCountdown() {
+    open func stopCountdown() {
         timer?.invalidate()
         timer = nil
         setTitle(finshTitle, for: .normal)
---
+++ App/AppBase/AppBase/AppViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AppViewController.swift
+//  AppBase
+//
+//  Created by LL on 2020/4/13.
+//  Copyright © 2020 hxxxs. All rights reserved.
+//
+
+import UIKit
+
+open class AppViewController: UIViewController {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
